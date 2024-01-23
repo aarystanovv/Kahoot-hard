2ac4ec7259f850ed1e65f4409a330be3aec2cebe
Tima <74903776+aarystanovv@users.noreply.github.com>
2024-01-23 22:40:25 +0600

    Initial commit

---
+++ src/main/java/com/example/demo1/FillIn.java
@@ -0,0 +1,27 @@
+package com.example.demo1;
+
+public class FillIn extends Questions {
+    @Override
+    public void setAnswer(String answer) {
+        super.answer=answer;
+    }
+
+    @Override
+    public void setDescription(String description) {
+        super.description=description;
+    }
+    @Override
+    public String getAnswer() {
+        return answer;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+
+    public String toString(){
+        return getDescription();
+    }
+}
